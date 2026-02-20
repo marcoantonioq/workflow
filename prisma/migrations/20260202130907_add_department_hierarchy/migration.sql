e2e7846ccffc975366f7b87b25d947684ed32e34
Marco Antônio <marco.aq7@gmail.com>
2026-02-19 23:32:59 -0300

    up

--- prisma/migrations/20260202130907_add_department_hierarchy/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "departments" ADD COLUMN     "parent_code" TEXT;
-
--- AddForeignKey
-ALTER TABLE "departments" ADD CONSTRAINT "departments_parent_code_fkey" FOREIGN KEY ("parent_code") REFERENCES "departments"("uorg_code") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20260202192040_add_department_documents/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "departments" ADD COLUMN     "documents" JSONB DEFAULT '[]';
-
--- AlterTable
-ALTER TABLE "workflows" ALTER COLUMN "documents" DROP NOT NULL,
-ALTER COLUMN "structure" DROP NOT NULL;
